5d2cd0dd2140923bfd3d5f8b3f64f995c1ea81dd
Lee Bissessar <leebissessar@gmail.com>
2025-01-23 16:59:23 -0400

    complete ex1.7-8

--- part1/getting-started/index.js
+++ part1/getting-started/index.js
@@ -1,6 +1,19 @@
 import crypto from 'crypto'
+import express from 'express'
 
 const randomString = crypto.randomUUID()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    const timestamp = new Date().toISOString()
+    res.send(`${timestamp}: ${randomString}`)
+})
+
+app.listen(port, () => {
+    console.log(`[server]: Server started in ${port}`)
+})
 
 setInterval(() => {
     const timestamp = new Date().toISOString()
