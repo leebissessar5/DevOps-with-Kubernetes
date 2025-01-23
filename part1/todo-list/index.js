d13426fbdb093233f5f7dcd1e26ff385df5fa0f5
Lee Bissessar <leebissessar@gmail.com>
2025-01-23 16:16:24 -0400

    complete ex1.5

--- part1/todo-list/index.js
+++ part1/todo-list/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`[server]: Server started in ${port}`);
